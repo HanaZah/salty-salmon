bcf3df4270b765717766235d87d566e32b953192
HanaZah <hana.zahalkova1992@gmail.com>
2026-03-22 09:26:29 +0100

    fix(integration test): add Oracle container for test connectivity

--- src/test/java/com/finadvise/crm/CrmApplicationTests.java
+++ src/test/java/com/finadvise/crm/CrmApplicationTests.java
@@ -2,10 +2,18 @@ package com.finadvise.crm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.oracle.OracleContainer;
 
 @SpringBootTest
-class CrmApplicationTests {
+@ActiveProfiles("test")
+class CrmApplicationTests  {
 
+	@Container
+	@ServiceConnection
+	static OracleContainer oracle = new OracleContainer("gvenzl/oracle-free:slim-faststart");
 	@Test
 	void contextLoads() {
 	}
